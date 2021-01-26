e9c92d662940462fbedf61c8fc44d86abeef3976
Aleksey Lyubushkin <5841459@gmail.com>
2021-01-26 15:24:21 +0300

    Programming logic of the traffic light (without constraints)

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var lightsView: [UIView]!
+    @IBOutlet var toggleButton: UIButton!
+    
+    var touchesStepper = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        for light in lightsView {
+            light.alpha = 0.3
+            light.layer.cornerRadius = 75
+        }
+        
+        toggleButton.layer.cornerRadius = 10
+        toggleButton.setTitle("START", for: .normal)
     }
 
-
+    @IBAction func toggleLightButtonPressed() {
+        for light in lightsView {
+            light.alpha = 0.3
+        }
+        
+        lightsView[touchesStepper].alpha = 1
+        
+        if touchesStepper == 2 {
+            touchesStepper = 0
+        } else {
+            touchesStepper += 1
+        }
+        
+        toggleButton.setTitle("NEXT", for: .normal)
+    }
+    
 }
 
