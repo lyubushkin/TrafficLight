49149582cff47ebf0b361b67b52445db22870365
Aleksey Lyubushkin <5841459@gmail.com>
2021-01-28 15:52:56 +0300

    Fix remarks from feedback

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -13,25 +13,26 @@ class ViewController: UIViewController {
     @IBOutlet var toggleButton: UIButton!
     
     var touchesStepper = 0
+    var stopLight = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         for light in lightsView {
             light.alpha = 0.3
-            light.layer.cornerRadius = 70
+            light.layer.cornerRadius = light.frame.width / 2
         }
         
         toggleButton.layer.cornerRadius = 10
-        toggleButton.setTitle("START", for: .normal)
+
     }
 
     @IBAction func toggleLightButtonPressed() {
-        for light in lightsView {
-            light.alpha = 0.3
-        }
         
+        lightsView[stopLight].alpha = 0.3
         lightsView[touchesStepper].alpha = 1
+        
+        stopLight = touchesStepper
         
         if touchesStepper == 2 {
             touchesStepper = 0
